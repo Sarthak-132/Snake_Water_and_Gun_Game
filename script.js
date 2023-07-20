6130765b63ab303e6215bb36e9f65b37dc5d715b
Sarthak <sarthakdhoke132@gmail.com>
2023-07-21 01:25:39 +0530

    logic written in script.js file

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-let input = document.getElementById('input');
 let btn = document.getElementById('btn');
 let output = document.getElementById('output');
+let computer = ['s','w','g']
+let computer_input = computer[Math.floor(Math.random()*computer.length)];
 
+btn.addEventListener("click", function() {
+    let user_input = document.getElementById('input').value;
+    if(user_input===''){
+        output.innerHTML = 'Type something';
+    }
+    else if(user_input === computer_input){
+        output.innerHTML = 'OOPS! Match Tie';
+    }
+    else if(user_input === 's' && computer_input === 'w'){
+        output.innerHTML = 'Congrats! You Won the Game';
+    }
+    else if(user_input === 's' && computer_input === 'g'){
+        output.innerHTML = 'Bad luck! Computer Won... Try again';
+    }
+    else if(user_input === 'w' && computer_input === 's'){
+        output.innerHTML = 'Bad luck! Computer Won... Try again';
+    }
+    else if(user_input === 'w' && computer_input === 'g'){
+        output.innerHTML = 'Congrats! You Won the Game';
+    }
+    else if(user_input === 'g' && computer_input === 'w'){
+        output.innerHTML = 'Bad luck! Computer Won... Try again';
+    }
+    else if(user_input === 'g' && computer_input === 's'){
+        output.innerHTML = 'Congrats! You Won the Game';
+    }
+});
 
 
